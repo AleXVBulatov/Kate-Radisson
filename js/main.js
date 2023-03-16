3421a1b7683845601a8783e6ef0e0a2814e3f27c
Alexey Bulatov <bulatov.a.v.89@gmail.com>
2023-03-16 13:56:08 +0200

    started header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+// ================= menu btn =================
+const menuBtn = document.querySelector(".nav__btn-toggle");
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav");
+
+menuBtn.addEventListener("click", () => {
+  menuIcon.classList.toggle("js-menu-icon-active");
+  nav.classList.toggle("nav--mobile");
+  document.body.classList.toggle("no-scroll");
+});
