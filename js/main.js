bae134d5fa631b94fdd143749cc3799a0470682b
Alexey Bulatov <bulatov.a.v.89@gmail.com>
2023-03-23 17:59:03 +0200

    finished header-adaptive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 // ================= menu btn =================
-const menuBtn = document.querySelector(".nav__btn-toggle");
-const menuIcon = document.querySelector(".menu-icon");
-const nav = document.querySelector(".nav");
+const refs = {
+  menuBtn: document.querySelector(".nav__btn-toggle"),
+  menuIcon: document.querySelector(".menu-icon"),
+  nav: document.querySelector(".nav"),
+  navList: document.querySelector(".nav__list"),
+  headerBlur: document.querySelector(".header__blur"),
+  // headerTitle: document.querySelector(".header__title"),
+};
+const { menuBtn, menuIcon, nav, navList, headerBlur, headerTitle } = refs;
 
 menuBtn.addEventListener("click", () => {
   menuIcon.classList.toggle("js-menu-icon-active");
   nav.classList.toggle("nav--mobile");
+  navList.classList.toggle("nav__list-active");
+  headerBlur.classList.toggle("none");
   document.body.classList.toggle("no-scroll");
 });
+
+// Перенос текста на новую строку
+// if (document.documentElement.clientWidth < 1109) {
+//   const brElem = document.createElement("br");
+//   headerTitle.firstElementChild.prepend(brElem);
+// }
